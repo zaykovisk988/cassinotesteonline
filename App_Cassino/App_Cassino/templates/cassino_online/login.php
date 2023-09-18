088367067faeaac310f3aba462e649aba6945e54
Seu Nome <popuptaria@gmail.com>
2023-09-18 11:33:12 -0300

    install html + php

---
+++ App_Cassino/App_Cassino/templates/cassino_online/login.php
@@ -0,0 +1,38 @@
+<?php
+// Configurações do banco de dados
+$servername = "containers-us-west-193.railway.app"; // Endereço do servidor MySQL
+$username = "root"; // Nome de usuário do MySQL
+$password = "1npAumMVaXN0NIm0lJi7"; // Senha do MySQL
+$dbname = "railway"; // Nome do banco de dados
+$port = 6698; // Porta do MySQL
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Verificar conexão
+if ($conn->connect_error) {
+  die("Conexão falhou: " . $conn->connect_error);
+}
+
+// Quando o formulário é enviado, verifica se o usuário existe no banco de dados.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ss", $username, $password);
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    echo "Login bem sucedido!";
+  } else {
+    echo "Nome de usuário ou senha inválidos.";
+  }
+
+  $stmt->close();
+}
+
+$conn->close();
+?>
