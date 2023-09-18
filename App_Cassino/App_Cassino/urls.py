925fb6283d26c5165041cf8737fab8a95063b196
jHONYm <jhonymoreira356@gmail.com>
2023-09-18 14:06:39 -0300

    main

--- App_Cassino/App_Cassino/urls.py
+++ App_Cassino/App_Cassino/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from MainGame import views
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
