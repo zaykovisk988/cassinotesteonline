dde50dba24fe76eb3ad4a8cbedf8dfaacc4b9d74
Seu Nome <popuptaria@gmail.com>
2023-09-18 15:14:44 -0300

    UPDATE FRONT END

--- cassino/cassino/asgi.py
+++ cassino/cassino/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nome_do_projeto project.
+ASGI config for Cassino project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nome_do_projeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cassino.settings')
 
 application = get_asgi_application()
