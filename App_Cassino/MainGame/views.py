7db6f1a4521d5b70110e12e8d1a5b4e5938b4cb0
joaopedro <joao.110122@gmail.com>
2023-09-18 11:30:59 -0300

    3

--- App_Cassino/MainGame/views.py
+++ App_Cassino/MainGame/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+
 def index(request):
-    return render ( request , 'index.html' )
+    return render(request, 'index.html')
