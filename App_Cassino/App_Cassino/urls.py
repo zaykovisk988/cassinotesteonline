28449986cf61cfefb415f0c219d052bb7e6ee1b6
Jhony Silva <Jhony>
2023-09-18 12:52:05 -0300

    index

--- App_Cassino/App_Cassino/urls.py
+++ App_Cassino/App_Cassino/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from MainGame import views
+from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
--- App_Cassino/MainGame/views.py
+++ App_Cassino/MainGame/views.py
@@ -3,3 +3,6 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request):
     return HttpResponse, ('index.html')
+
+def login(request):
+    return render (request, 'login.html')
