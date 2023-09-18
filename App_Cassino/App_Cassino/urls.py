774f216a4202b16f6888ba998430779dbd48a167
Jhony Silva <Jhony>
2023-09-18 12:14:11 -0300

    login

--- App_Cassino/App_Cassino/urls.py
+++ App_Cassino/App_Cassino/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from App_Cassino import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.login.html, name='login'),
 ]
