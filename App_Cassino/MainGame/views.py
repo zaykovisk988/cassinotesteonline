b56d096648ef93e779e08c2953486bf66ba85b1e
jHONYm <jhonymoreira356@gmail.com>
2023-09-18 13:18:38 -0300

    request

--- App_Cassino/MainGame/views.py
+++ App_Cassino/MainGame/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def index(request):
-    return HttpResponse, ('index.html')
+    return render (request, 'index.html')
 
 def login(request):
     return render (request, 'login.html')
